33652decafaa7dc850c44bddd8477d1bd0f96136
Cedric Champeau <cedric.champeau@gmail.com>
2025-01-21 18:23:55 +0100

    Move Java configuration to the base Java plugin
    So that it can be reused by modules which don't use Micronaut.

--- buildSrc/src/main/kotlin/io.micronaut.java-base.gradle.kts
+++ buildSrc/src/main/kotlin/io.micronaut.java-base.gradle.kts
@@ -1,7 +1,8 @@
-import org.gradle.kotlin.dsl.repositories
-
 plugins {
-    `java`
+    java
+    checkstyle // https://docs.gradle.org/current/userguide/checkstyle_plugin.html
+    jacoco
+    id("com.diffplug.spotless")
 }
 
 java {
@@ -11,6 +12,44 @@ java {
 
 group = project.findProperty("mavenGroup") as String
 version = project.findProperty("projectVersion") as String
+
 repositories {
     mavenCentral()
+}
+
+
+spotless {
+    java {
+        licenseHeaderFile("$rootDir/config/spotless.license.java")
+    }
+}
+
+val mn = extensions.findByType<VersionCatalogsExtension>()!!.named("mn")
+
+dependencies {
+    // Logging
+    testRuntimeOnly(mn.findLibrary("logback.classic").get())
+
+    // JUnit Testing
+    testImplementation(mn.findLibrary("junit.jupiter.api").get())
+
+}
+
+tasks {
+    withType<Test> {
+        useJUnitPlatform()
+    }
+    jacocoTestReport {
+        dependsOn(tasks.test)
+        reports {
+            xml.required.set(true)
+            csv.required.set(false)
+            html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+        }
+    }
+
+    test {
+        finalizedBy(tasks.jacocoTestReport)
+    }
+
 }
--- buildSrc/src/main/kotlin/io.micronaut.library.micronaut-java-library.gradle.kts
+++ buildSrc/src/main/kotlin/io.micronaut.library.micronaut-java-library.gradle.kts
@@ -1,7 +1,4 @@
 plugins {
-    checkstyle // https://docs.gradle.org/current/userguide/checkstyle_plugin.html
-    jacoco
-    id("com.diffplug.spotless")
     id("io.micronaut.java-base")
     id("io.micronaut.library")
 }
@@ -14,32 +11,9 @@ micronaut {
         annotations.add("${project.findProperty("mavenGroup") as String}.*")
     }
 }
+
+val mn = extensions.findByType<VersionCatalogsExtension>()!!.named("mn")
+
 dependencies {
-    // Logging
-    testRuntimeOnly("ch.qos.logback:logback-classic")
-
-    // JUnit Testing
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("io.micronaut.test:micronaut-test-junit5")
-
-}
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
-
-tasks.test {
-    finalizedBy(tasks.jacocoTestReport)
-}
-tasks.jacocoTestReport {
-    dependsOn(tasks.test)
-    reports {
-        xml.required.set(true)
-        csv.required.set(false)
-        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
-    }
-}
-spotless {
-    java {
-        licenseHeaderFile("$rootDir/config/spotless.license.java")
-    }
+    testImplementation(mn.findLibrary("micronaut.test.junit5").get())
 }
