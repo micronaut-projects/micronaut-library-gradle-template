9ea2a7791ad56e4388e55e46a97d67c53b5deaa9
Cédric Champeau <cedric.champeau@gmail.com>
2025-01-21 21:33:55 +0100

    Template improvements (#7)
    * Use the platform plugin
    
    This does the same to import the `mn` catalog, but without the bloat :)
    
    * Extract a Java base plugin
    
    * Remove redundant micronaut version configuration
    
    * Extract maven publishing in its own plugin
    
    This makes it possible to reuse the publishing part for other modules
    which wouldn't necessarily be Micronaut libraries, through composition.
    
    * Move Java configuration to the base Java plugin
    
    So that it can be reused by modules which don't use Micronaut.

--- buildSrc/src/main/kotlin/io.micronaut.library.micronaut-java-library.gradle.kts
+++ buildSrc/src/main/kotlin/io.micronaut.library.micronaut-java-library.gradle.kts
@@ -1,18 +1,9 @@
 plugins {
-    checkstyle // https://docs.gradle.org/current/userguide/checkstyle_plugin.html
-    jacoco
-    id("com.diffplug.spotless")
-    id("org.graalvm.buildtools.native")
+    id("io.micronaut.java-base")
     id("io.micronaut.library")
 }
-group = project.findProperty("mavenGroup") as String
-version = project.findProperty("projectVersion") as String
-repositories {
-    mavenCentral()
-}
-val micronautVersion: String by project
+
 micronaut {
-    version.set(micronautVersion)
     processing {
         module.set(project.name)
         group.set(project.findProperty("mavenGroup") as String)
@@ -20,32 +11,9 @@ micronaut {
         annotations.add("${project.findProperty("mavenGroup") as String}.*")
     }
 }
+
+val mn = extensions.findByType<VersionCatalogsExtension>()!!.named("mn")
+
 dependencies {
-    // Logging
-    testRuntimeOnly("ch.qos.logback:logback-classic")
-
-    // JUnit Testing
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("io.micronaut.test:micronaut-test-junit5")
-
-}
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
-
-tasks.test {
-    finalizedBy(tasks.jacocoTestReport)
-}
-tasks.jacocoTestReport {
-    dependsOn(tasks.test)
-    reports {
-        xml.required.set(true)
-        csv.required.set(false)
-        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
-    }
-}
-spotless {
-    java {
-        licenseHeaderFile("$rootDir/config/spotless.license.java")
-    }
+    testImplementation(mn.findLibrary("micronaut.test.junit5").get())
 }
